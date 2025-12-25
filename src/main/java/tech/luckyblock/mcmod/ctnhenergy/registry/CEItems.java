b29cc1c15cf1170ecfcec148286642e207b30726
lucky_block <1774432258@qq.com>
2025-12-26 00:08:49 +0800

    eu元件纹理 修一个bug

--- src/main/java/tech/luckyblock/mcmod/ctnhenergy/registry/CEItems.java
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/registry/CEItems.java
@@ -6,6 +6,7 @@ import com.gregtechceu.gtceu.api.item.component.IItemComponent;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
 import com.tterrag.registrate.util.nullness.NonNullConsumer;
+import net.minecraft.world.item.Item;
 import tech.luckyblock.mcmod.ctnhenergy.common.item.DynamoCardItem;
 import tech.luckyblock.mcmod.ctnhenergy.common.item.EUCellItem;
 import tech.luckyblock.mcmod.ctnhenergy.common.item.EUCellStats;
@@ -20,6 +21,7 @@ public class CEItems {
 
     public static ItemEntry<EUCellItem>[] EU_CELL = new ItemEntry[TIER_COUNT];
     public static ItemEntry<DynamoCardItem> DYNAMO_CARD;
+    public static ItemEntry<Item> EU_CELL_HOUSING;
 
     public static void init(){
         registerEUCell();
@@ -27,6 +29,12 @@ public class CEItems {
                 .cnlang("动力卡")
                 .lang("Dynamo Card")
                 .model(NonNullBiConsumer.noop())
+                .register();
+
+        EU_CELL_HOUSING = REGISTRATE.item("eu_cell_housing",Item::new)
+                .cnlang("EU存储元件外壳")
+                .model((ctx, prov) ->
+                        prov.generated(ctx::getEntry, prov.modLoc("item/cells/empty")))
                 .register();
     }
 
@@ -36,7 +44,8 @@ public class CEItems {
                     .item(VN[tier].toLowerCase() + "_eu_cell", EUCellItem::new)
                     .cnlang(VNF[tier] + "§r ME EU存储元件")
                     .lang(VNF[tier] + "§r ME EU Storage Cell")
-                    .model(NonNullBiConsumer.noop())
+                    .model((ctx, prov) ->
+                            prov.generated(ctx::getEntry, prov.modLoc("item/cells/"+VN[tier].toLowerCase())))
                     .onRegister(attach(EUCellStats.createCell(tier)))
                     .register();
         }
