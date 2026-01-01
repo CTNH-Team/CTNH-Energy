02f34cb1e96de55e095cef806c1cfe57f6f84a96
lucky_block <1774432258@qq.com>
2026-01-01 13:18:37 +0800

    EUkey纹理 更新许可证

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # CTNH Energy
 
+## License
+All code is licensed under the [GNU LGPL v3](https://www.gnu.org/licenses/lgpl-3.0.en.html).
+
+All artwork (images, textures, models, animations, etc.) is licensed under
+the [Creative Commons Attribution-NonCommercial 4.0 International License](http://creativecommons.org/licenses/by-nc/4.0/)
+, unless stated otherwise.
+
+textures/block/casings/assembler_matrix_frame.png and textures/block/casings/assembler_matrix_frame_ctm.png are derivative work based on textures from ExpandedAE and are subject to the GNU LGPL v3 license of ExpandedAE. 
+ 
 ## Credits
 ### Reference
 The implementation of smart blocking mode of pattern provider referenced [ExpandedAE](https://github.com/ko-lja/expandedae?tab=readme-ov-file)  
 The implementation of Quantum Computer referenced [AdvancedAE](https://github.com/pedroksl/AdvancedAE)
-
+The implementation of AppEU referenced [Applied Flux](https://github.com/GlodBlock/ExtendedAE/tree/appflux/1.21.1-neoforge)
 ### Dependents
 AE2  
 GTM  
-The implementation of Advanced ME Pattern Buffer is based on [ProgrammedCircuitCard](https://github.com/yuuki1293/ProgrammedCircuitCard)  
+The implementation of ME Advanced Pattern Buffer is based on [ProgrammedCircuitCard](https://github.com/yuuki1293/ProgrammedCircuitCard)  
--- src/main/java/tech/luckyblock/mcmod/ctnhenergy/client/render/EUKeyRenderHandler.java
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/client/render/EUKeyRenderHandler.java
@@ -6,10 +6,15 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import tech.luckyblock.mcmod.ctnhenergy.CTNHEnergy;
 import tech.luckyblock.mcmod.ctnhenergy.common.me.key.EUKey;
 
 public class EUKeyRenderHandler implements AEKeyRenderHandler<EUKey> {
@@ -18,7 +23,7 @@ public class EUKeyRenderHandler implements AEKeyRenderHandler<EUKey> {
 
     @Override
     public void drawInGui(Minecraft minecraft, GuiGraphics guiGraphics, int x, int y, EUKey stack) {
-        Blitter.sprite(Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(ResourceLocation.parse("forge:block/milk_flowing")))
+        Blitter.sprite(Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(CTNHEnergy.id("block/eu")))
                 .blending(false)
                 .dest(x, y, 16, 16)
                 .blit(guiGraphics);
@@ -27,6 +32,53 @@ public class EUKeyRenderHandler implements AEKeyRenderHandler<EUKey> {
     @Override
     public void drawOnBlockFace(PoseStack poseStack, MultiBufferSource buffers, EUKey what, float scale, int combinedLight, Level level) {
 
+        var texture = CTNHEnergy.id("block/eu");
+        var color = 0xFFFFFFFF;
+        var sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS)
+                .apply(texture);
+
+        poseStack.pushPose();
+        // Push it out of the block face a bit to avoid z-fighting
+        poseStack.translate(0, 0, 0.01f);
+
+        var buffer = buffers.getBuffer(RenderType.solid());
+
+        // y is flipped here
+        var x0 = -scale / 2;
+        var y0 = scale / 2;
+        var x1 = scale / 2;
+        var y1 = -scale / 2;
+
+        var transform = poseStack.last().pose();
+        buffer.vertex(transform, x0, y1, 0)
+                .color(color)
+                .uv(sprite.getU0(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y1, 0)
+                .color(color)
+                .uv(sprite.getU1(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y0, 0)
+                .color(color)
+                .uv(sprite.getU1(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x0, y0, 0)
+                .color(color)
+                .uv(sprite.getU0(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        poseStack.popPose();
     }
 
     @Override
