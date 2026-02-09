444d580d739fc2da96302f9c07c92af1b0a36851
lucky_block <1774432258@qq.com>
2026-02-09 20:57:01 +0800

    禁止omni盘存EU

---
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/omni/AEUniversalCellInventoryMixin.java
@@ -0,0 +1,17 @@
+package tech.luckyblock.mcmod.ctnhenergy.mixin.omni;
+
+import appeng.api.stacks.AEKey;
+import com.wintercogs.ae2omnicells.common.me.AEUniversalCellInventory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tech.luckyblock.mcmod.ctnhenergy.common.me.key.EUKeyType;
+
+@Mixin(value = AEUniversalCellInventory.class, remap = false)
+public class AEUniversalCellInventoryMixin {
+    @Inject(method = "matchesPartitionAndUpgrades", at = @At("HEAD"), cancellable = true)
+    void rejectEU(AEKey what, CallbackInfoReturnable<Boolean> cir){
+        if(what.getType() == EUKeyType.INSTANCE) cir.setReturnValue(false);
+    }
+}
