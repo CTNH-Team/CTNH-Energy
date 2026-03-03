cd38d99fa2cceadc5bca7828de8bbb9c3b36098a
lucky_block <1774432258@qq.com>
2026-03-03 22:12:27 +0800

    fix: 修复emi兼容破坏ae原本的交互逻辑,使合成树兼容emi

--- src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/ae2/emi/EmiAeBaseScreenStackProviderMixin.java
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/ae2/emi/EmiAeBaseScreenStackProviderMixin.java
@@ -1,6 +1,11 @@
 package tech.luckyblock.mcmod.ctnhenergy.mixin.ae2.emi;
 
 import appeng.api.stacks.GenericStack;
+import appeng.client.gui.me.crafting.CraftConfirmScreen;
+import appeng.client.gui.me.crafting.CraftingCPUScreen;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.neuvillette.ae2ct.gui.CraftingTreeScreen;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -13,7 +18,7 @@ public class EmiAeBaseScreenStackProviderMixin {
     }
 
     @ModifyArg(method = "getStackAt", at = @At(value = "INVOKE", target = "Ldev/emi/emi/api/stack/EmiStackInteraction;<init>(Ldev/emi/emi/api/stack/EmiIngredient;Ldev/emi/emi/api/recipe/EmiRecipe;Z)V"), index = 2)
-    boolean allowClick(boolean clickable){
-        return true;
+    boolean allowClick(boolean clickable, @Local(argsOnly = true) Screen screen){
+        return screen instanceof CraftConfirmScreen || screen instanceof CraftingCPUScreen || screen instanceof CraftingTreeScreen;
     }
 }
---
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/ae2ct/CraftingTreeWidgetAccessor.java
@@ -0,0 +1,20 @@
+package tech.luckyblock.mcmod.ctnhenergy.mixin.ae2ct;
+
+import com.neuvillette.ae2ct.api.CraftingTreeHelper;
+import com.neuvillette.ae2ct.gui.CraftingTreeWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.awt.*;
+
+@Mixin(value = CraftingTreeWidget.class, remap = false)
+public interface CraftingTreeWidgetAccessor {
+    @Accessor(value = "_nodeManager")
+    CraftingTreeHelper.NodeManager getNodeManager();
+
+    @Invoker
+    Point invokeGetMousePoint(double xCoord, double yCoord);
+
+
+}
