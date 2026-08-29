9f52a6dbb0bd6051d8cba72290d3f9ceb1e55d5a
lucky_block <1774432258@qq.com>
2026-08-29 22:24:28 +0800

    为没有编程电路的配方自动添加0号电路

--- src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/ae2/emi/EmiStackHelperCircuitMixin.java
+++ src/main/java/tech/luckyblock/mcmod/ctnhenergy/mixin/ae2/emi/EmiStackHelperCircuitMixin.java
@@ -1,6 +1,8 @@
 package tech.luckyblock.mcmod.ctnhenergy.mixin.ae2.emi;
 
 import com.gregtechceu.gtceu.common.data.GTItems;
+import com.gregtechceu.gtceu.common.item.IntCircuitBehaviour;
+import com.gregtechceu.gtceu.integration.emi.recipe.GTEmiRecipe;
 
 import appeng.api.stacks.GenericStack;
 import appeng.integration.modules.emi.EmiStackHelper;
@@ -21,19 +23,20 @@ public abstract class EmiStackHelperCircuitMixin {
     private static void addCircuit(EmiRecipe recipe, CallbackInfoReturnable<List<List<GenericStack>>> cir) {
         if (CEConfig.INSTANCE == null || !CEConfig.INSTANCE.client.enableCircuitInPatternEncoding) return;
 
-        var circuit = recipe.getCatalysts().stream()
-                .filter(entry -> !entry.getEmiStacks().isEmpty())
-                .map(entry -> entry.getEmiStacks().get(0).getItemStack())
-                .filter(stack -> stack.is(GTItems.PROGRAMMED_CIRCUIT.asItem()))
-                .findFirst();
-        if (circuit.isEmpty()) return;
+        if (recipe instanceof GTEmiRecipe) {
+            var circuit = recipe.getCatalysts().stream()
+                    .filter(entry -> !entry.getEmiStacks().isEmpty())
+                    .map(entry -> entry.getEmiStacks().get(0).getItemStack())
+                    .filter(stack -> stack.is(GTItems.PROGRAMMED_CIRCUIT.asItem()))
+                    .findFirst().orElse(IntCircuitBehaviour.stack(0));
 
-        var generic = GenericStack.fromItemStack(circuit.get());
+            var generic = GenericStack.fromItemStack(circuit);
 
-        if (generic != null) {
-            var list = new ArrayList<>(cir.getReturnValue());
-            list.add(List.of(generic));
-            cir.setReturnValue(list);
+            if (generic != null) {
+                var list = new ArrayList<>(cir.getReturnValue());
+                list.add(List.of(generic));
+                cir.setReturnValue(list);
+            }
         }
     }
 }
